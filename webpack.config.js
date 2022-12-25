ec4b94e50ae3b8590a96dfbe0ccc5c0ba0424351
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-12-25 16:32:02 +0530

    Basic setup 1

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        port: 3000,
+        hot: true,
+        open: true,
+        compress: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|png|svg)/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Weather App',
+            filename: 'index.html',
+            template: './src/template.html',
+        }),
+    ],
+}
