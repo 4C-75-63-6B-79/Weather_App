ce7cf738fe4105b51e4c10ee50a8c561a47444e3
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-03 22:29:46 +0530

    Added live link in readme and made the link https for fetch

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Jank Weather Thing
 
+Live Link https://4c-75-63-6b-79.github.io/Weather_App/
+
 1. This a weather app where you enter the name of the city, country or place you want to know the weather of and then hit enter to get the weather.
 2. You get the current weather of the place and to see the hourly forecast of that place you slider the slider.
 3. You can also get the forecast of weather for the next 2 day form the day cards which are below the slider.
--- src/modules/getWeatherInfo.js
+++ src/modules/getWeatherInfo.js
@@ -3,7 +3,7 @@ import updateControl from "./updateInfo.js";
 
 export default async function getWeatherInfo(locationName) {
     try {
-        let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=053785ea687c447299e70926222812&q=${locationName}&days=3&aqi=no&alerts=no`, {mode: 'cors'});
+        let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=053785ea687c447299e70926222812&q=${locationName}&days=3&aqi=no&alerts=no`, {mode: 'cors'});
         // console.log(response.status);
         // console.log(response.ok);
         if(response.status == '200') {
